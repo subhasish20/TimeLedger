da868b1102acd968ddd0907b63ea2b6e39d0194b
Subhasish Jena <subhasishjena8280@gmail.com>
2026-09-20 08:38:39 +0530

    run.sh comment added

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
-
 #!/bin/bash
 
-python3 main.py
+# Launches the Screen Time Tracker application using Python 3.
+#
+# Grant execution permission to the script:
+# chmod +x run.sh
+#
+# Execute the script:
+# ./run.sh
 
+python3 main.py
